e49235bd1b25a30230a5433de43c1234439240d3
Noriel Achero <acheronorielr@gmail.com>
2025-08-05 15:42:45 +0800

    first commit

---
+++ dump/arithmetic.php
@@ -0,0 +1,41 @@
+<?php
+
+    // + - * / ** %
+
+    $x = 10;
+    $y = 2;
+    $z = null;
+
+    $z = $y + $x;
+    $z = $y - $x;
+    $z = $y * $x;
+    $z = $y / $x;
+    $z = $y ** $x; //Raised^
+    $z = $y % $x;
+
+    // echo $z;
+
+
+    // Increment and Decrement Operators
+
+    $counter = 0;
+
+    $counter++;
+        echo "{$counter} <br>";
+    $counter--;
+        echo "{$counter} <br>";
+    $counter+=100;
+        echo "{$counter} <br>";
+    $counter-=50;
+        echo "{$counter} <br>";
+
+
+    // Operator Precedence PEMDAS
+    // ()
+    // **
+    // * / %
+    // + -
+
+
+
+?>
---
+++ dump/variables.php
@@ -0,0 +1,47 @@
+<?php
+
+    // Variables
+
+    // String
+    $name = "Noriel Achero";
+    $food = "Siomai";
+    $email = "norielachero@gmail.com";
+
+    echo "Hello, {$name} <br>";
+    echo "You love {$food} <br>";
+    echo "Your email is {$email}<br>";
+
+    // Integers
+
+    $age = 22;
+    $users = 2;
+    $quantity = 3;
+    
+    echo "You are {$age}<br>";
+    echo "There are {$users}<br>";
+    echo "There are {$quantity}<br>";
+
+    // Float
+
+    $grade = 1.15;
+    $price = 200.50;
+
+    echo "Your grade is {$grade}<br>";
+    echo "Price is \${$price}<br>";
+
+    // Boolean
+
+    $employed = True;
+    $online = False;
+
+    $total = $quantity*$price;
+
+    echo "Your total is: \${$total}";
+
+ 
+    
+
+
+
+
+?>
