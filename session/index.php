9ba64d90c95adb14f0b9ee87bef06605fbe0cac8
Noriel Achero <acheronorielr@gmail.com>
2025-08-12 11:02:05 +0800

    asdf

--- session/index.php
+++ session/index.php
@@ -11,6 +11,15 @@
     <title>Document</title>
 </head>
 <body>
-    This is the login page
+    This is the login page <br>
+    <a href= "home.php"> Home </a>
 </body>
 </html>
+<?php
+
+    $_SESSION["username"] = "orenji";
+    $_SESSION["password"] = "12345";
+
+    echo "<br>" . $_SESSION['username'] ."<br> " .  $_SESSION['password'];
+
+?>
