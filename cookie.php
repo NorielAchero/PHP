d5811436972ea702d363d299dd13143762079c22
Noriel Achero <acheronorielr@gmail.com>
2025-08-12 10:55:25 +0800

    123

---
+++ cookie.php
@@ -0,0 +1,18 @@
+<?php
+
+    setcookie("fav_food", "pizza", time() + (86400*2), "/");
+    setcookie("fav_drink", "coffee", time() + (86400*3), "/");
+    setcookie("fav_cat", "umi", time() + (86400*4), "/");
+
+    foreach($_COOKIE as $key => $value){
+        echo "{$key} = {$value} <br>";
+    }
+
+    if(isset($_COOKIE["fav_food"])){
+        echo "Buy some {$_COOKIE["fav_food"]} !!!";
+    }
+    else{
+        echo "Haha";
+    }
+
+?>
